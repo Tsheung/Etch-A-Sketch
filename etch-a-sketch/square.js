271478ee53884fcb5a6b06a654d8dc43a373ff12
Tsheung <tobylsheung@gmail.com>
2022-01-09 22:16:09 -0500

    html structure

---
+++ etch-a-sketch/square.js
@@ -0,0 +1,11 @@
+const box = document.createElement('div');
+let square = document.createElement('div');
+square.classList.add('square');
+document.body.appendChild(square);
+
+/*
+function rectangle(squareNum) {
+
+}
+
+*/
