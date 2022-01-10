f30e3209a50456f2142dbe50ca6f851752670a6b
Tsheung <tobylsheung@gmail.com>
2022-01-09 22:46:57 -0500

    Add grid of squares

--- etch-a-sketch/square.js
+++ etch-a-sketch/square.js
@@ -1,11 +1,20 @@
+//Create overall box container
 const box = document.createElement('div');
-let square = document.createElement('div');
-square.classList.add('square');
-document.body.appendChild(square);
+box.classList.add('box');
+document.body.appendChild(box);
 
-/*
+//Creates square class
 function rectangle(squareNum) {
-
+    for(let i = 0; i < squareNum; i++) {
+        for(let j = 0; j < squareNum; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            box.appendChild(square);
+        }
+        const br = document.createElement('br');
+        box.appendChild(br);
+    }
 }
 
-*/
+
+rectangle(4);
